7c8230531cd14aaee48c26075ff3e23ba0b762f9
Dengfeng Liu <liudf0716@qq.com>
2021-08-10 11:09:41 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -20,7 +20,7 @@ module.exports = {
         // `npm run build --report`
         // Set to `true` or `false` to always turn it on or off
         bundleAnalyzerReport: process.env.npm_config_report,
-        webServer: 'https://wifidog.kunteng.org.cn'
+        webServer: 'http://127.0.0.1:8001'
     },
     dev: {
         env: require('./dev.env'),
@@ -30,7 +30,7 @@ module.exports = {
         assetsPublicPath: '/',
         proxyTable: {
             '/api':{
-                target:'http://wifidog.kunteng.org.cn:8001',
+                target:'http://127.0.0.1:8001',
                 changeOrigin:true,
                 pathRewrite:{
                     '/api':''
