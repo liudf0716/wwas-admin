bd9fdb3a019161b04fe3b3ec930c7313f3dec160
staylightblow8 <liudf0716@gmail.com>
2024-01-11 15:14:03 +0800

    change webServer's default value
    Signed-off-by: staylightblow8 <liudf0716@gmail.com>

--- config/index.js
+++ config/index.js
@@ -20,7 +20,7 @@ module.exports = {
         // `npm run build --report`
         // Set to `true` or `false` to always turn it on or off
         bundleAnalyzerReport: process.env.npm_config_report,
-        webServer: process.env.AUTH_DOMAIN || 'http://wifidogx.online:8001'
+        webServer: process.env.AUTH_DOMAIN || 'http://wifidogx.online'
     },
     dev: {
         env: require('./dev.env'),
